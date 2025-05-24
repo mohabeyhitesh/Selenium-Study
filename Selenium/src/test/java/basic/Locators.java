edcef4819b6f9664f81e71b26ca91d6aee5b3d17
HiteshMohbey <hiteshmohbey787@gmail.com>
2025-05-25 02:14:14 +0530

    remove space commit 2

--- Selenium/src/test/java/basic/Locators.java
+++ Selenium/src/test/java/basic/Locators.java
@@ -26,7 +26,6 @@ public class Locators {
 		
 //		Dynamic Xpath 
 		
-		
 //		driver.findElement(By.xpath("//button[contains(@id,'u_0_5_')]")).click();
 //		driver.findElement(By.xpath("//input[starts-with(@name,'em')]")).sendKeys("hitesh");
 		
